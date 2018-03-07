3bffc58660f51cb6aaba70767c8287c986897899
Colin Rood <crrood@gmail.com>
2018-03-06 18:12:28 -0800

    save to db working

--- notifications.py
+++ notifications.py
@@ -3,13 +3,18 @@ import json
 
 # DB connection
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from schema_notification import Notification
 
 # Flask
 from flask import Flask, request
 
+# initialize routing
 app = Flask(__name__)
+
+# initialize DB connection
 engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:tgpli8sc2f@localhost:5432/postgres")
+Session = sessionmaker(bind=engine)
 
 # save notifications to DB
 def save_to_db(json_data):
@@ -17,18 +22,26 @@ def save_to_db(json_data):
     # pull list of notifications from JSON
     items = json_data["notificationItems"]
 
+    # create a new DB session
+    session = Session()
+
     # parse notifications for entry into DB
     for item in items:
         item = item["NotificationRequestItem"]
-        #data_to_insert = {}
+
+        # create new notification object to insert into DB
         notification = Notification(rawData=item)
+
+        # iterate through object and populate row data
         for column in Notification.__table__.columns:
             if column.name in item.keys():
-                #data_to_insert[column] = item[column]
                 setattr(notification, column.name, item[column.name])
 
-        # insert notification into DB
+        # insert notification into list to be added to DB
+        session.add(notification)
 
+    # commit to DB
+    session.commit()
     
     return notification.__repr__()
 
